d0849c3199171771c7242437a5a9fd38643f39d1
Fatima <75587225+Fatialnd@users.noreply.github.com>
2022-09-12 08:40:01 +0200

    using custom events as a web component API

--- script.js
+++ script.js
@@ -1,34 +1,36 @@
-class MyCustomElement extends HTMLElement {
+class MyWelcome extends HTMLElement {
   constructor() {
     super();
-    this.addEventListener("click", () => alert("Custom Element Clicked"));
+    this.attachShadow({ mode: "open" });
+    this.shadowRoot.innerHTML = `
+                <h1>Welcome <span id="username">Developer</span></h1>
+                <div><input id="usernameField"></div><br/>
+                <div><button id="changeUsername">Change username</button></div>
+        `;
+
+    this.userName = this.shadowRoot.querySelector("#username");
+    this.input = this.shadowRoot.querySelector("#usernameField");
+    this.submitButton = this.shadowRoot.querySelector("#changeUsername");
+
+    this.init();
   }
 
-  connectedCallback() {
-    this.innerHTML = "<h1>Hello Custom Element</h1>";
+  init() {
+    this.submitButton.addEventListener("click", () => {
+      this.userName.innerText = this.input.value;
+      this.emitExternalEvent("username-changed", this.input.value);
+    });
+  }
+
+  emitExternalEvent(eventName, payload) {
+    const customEvent = new CustomEvent(eventName, { detail: payload });
+    this.dispatchEvent(customEvent);
   }
 }
 
-window.customElements.define("my-custom-element", MyCustomElement);
-
-const templateString = `
-    <div>
-        <slot name="slot1"></slot>
-    </div>
-    <form>
-        <input/>
-    </form>
-    <slot name="slot2"></slot>
-    `;
-    const template = document.createElement('template');
-    template.innerHTML = templateString;
-    class CustomElement extends HTMLElement {
-        constructor() {
-            super();
-            this.attachShadow({mode: "open"});
-            this.shadowRoot
-                .appendChild(template.content.cloneNode(true));
-        }
-
-    }
-    customElements.define('custom-element', CustomElement);
+customElements.define("my-welcome", MyWelcome);
+const myWelcomeComponent = document.createElement("my-welcome");
+document.body.appendChild(myWelcomeComponent);
+myWelcomeComponent.addEventListener("username-changed", (event) => {
+  console.log(event.detail);
+});
